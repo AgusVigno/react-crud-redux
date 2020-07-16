6ac27bbd080ef50f9c813d0623e5b38e6a149840
Agustin Vignolo <agus88lp@gmail.com>
2020-07-16 18:55:14 -0300

    edicion de productos

--- src/actions/productoActions.js
+++ src/actions/productoActions.js
@@ -7,7 +7,11 @@ import {
     DESCARGA_PRODUCTO_ERROR,
     OBTENER_PRODUCTO_ELIMINAR,
     PRODUCTO_ELIMINADO_EXITO,
-    PRODUCTO_ELIMINADO_ERROR
+    PRODUCTO_ELIMINADO_ERROR,
+    OBTENER_PRODUCTO_EDITAR,
+    COMENZAR_EDICION_PRODUCTO,
+    PRODUCTO_EDITADO_EXITO, 
+    PRODUCTO_EDITADO_ERROR
 } from '../types';
 import clienteAxios from '../config/axios';
 import Swal from 'sweetalert2';
@@ -76,17 +80,14 @@ export function obtenerProductosAction(){
         }
     }
 }
-
 const descargarProductos = () => ({
     type: COMENZAR_DESCARGA_PRODUCTO,
     payload: true
 });
-
 const descargarProductosExitosa = (productos) => ({
     type: DESCARGA_PRODUCTO_EXITO,
     payload: productos
 });
-
 const descargarProductosError = () => ({
     type: DESCARGA_PRODUCTO_ERROR,
     payload: true
@@ -112,7 +113,6 @@ export function borrarProductoAction(id) {
         }
     }
 }
-
 const obtenerProductoEliminar = (id) => ({
     type: OBTENER_PRODUCTO_ELIMINAR,
     payload: id
@@ -125,4 +125,39 @@ const eliminarProductoExito = () => ({
 const eliminarProductoError = () => ({
     type: PRODUCTO_ELIMINADO_ERROR,
     payload: true
+});
+
+//colocar producto en edicion
+export function obtenerProductoEditar(producto){
+    return (dispatch) => {
+        dispatch( obtenerProductoEditarAction(producto));
+    }
+}
+const obtenerProductoEditarAction = (producto) => ({
+    type: OBTENER_PRODUCTO_EDITAR,
+    payload: producto
+});
+
+//editar un producto en la API y el state
+export function editarProductoAction(producto){
+    return async (dispatch) => {
+        dispatch( editarProducto() );
+        try {
+            await clienteAxios.put(`/productos/${producto.id}`, producto);
+            dispatch( editarProductoExito() );
+        } catch (error) {
+            console.log(error);
+            dispatch( editarProductoError() );
+        }
+    }
+}
+const editarProducto = () => ({
+    type: COMENZAR_EDICION_PRODUCTO
+});
+const editarProductoExito = (producto) => ({
+    type: PRODUCTO_EDITADO_EXITO,
+    payload: producto
+});
+const editarProductoError = () => ({
+    type: PRODUCTO_EDITADO_ERROR
 });
--- src/components/EditarProducto.js
+++ src/components/EditarProducto.js
@@ -1,29 +1,74 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import {useDispatch, useSelector} from 'react-redux';
+import {editarProductoAction} from '../actions/productoActions';
+import {useHistory} from 'react-router-dom';
 
 const EditarProducto = () => {
+    const dispatch = useDispatch();
+    const history = useHistory();
+
+    //nuevo state del producto
+    const [producto, guardarProducto] = useState({
+        nombre: '',
+        precio: 0
+    });
+
+    //producto a editar
+    const productoeditar = useSelector(state => state.productos.productoeditar);
+    
+    
+    //llenar el state automaticamente
+    useEffect(() => {
+        guardarProducto(productoeditar);
+    }, [productoeditar]);
+    
+    //leer los datos del formulario
+    const onChangeFormulario = e => {
+        guardarProducto({
+            ...producto,
+            [e.target.name] : e.target.value
+        });
+    }
+
+    const {nombre, precio} = producto;
+    
+    const submitEditarProducto = e => {
+        e.preventDefault();
+        
+        dispatch( editarProductoAction(producto) );
+
+        history.push('/');
+    }
+
     return (  
-<div className="row justify-content-center">
+        <div className="row justify-content-center">
             <div className="col-md-8">
                 <div className="card">
                     <div className="card-body">
                         <h2 className="text-center mb-4 font-weight-bold">
                             Editar Producto
                         </h2>
-                        <form>
+                        <form
+                            onSubmit={ () => submitEditarProducto }
+                        >
                             <div className="form-group">
                                 <label>Nombre Producto</label>
                                 <input
                                     type="text"
                                     name="nombre"
-                                    placeholder="Nombre Producto"
+                                    placeholder="Nombre producto"
+                                    value={nombre}
                                     className="form-control"
+                                    onChange={onChangeFormulario}
                                 />
                                 <label>Precio Producto</label>
                                 <input
                                     type="number"
                                     name="precio"
-                                    placeholder="Precio Producto"
+                                    placeholder="Precio producto"
+                                    value={precio}
                                     className="form-control"
+                                    onChange={onChangeFormulario}
                                 />
                             </div>
                             <button
